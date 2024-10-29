3d86e9881928e55ec5277fdeccda8ed00828be71
Gustavo <gustavo@localhost.localdomain>
2024-10-28 23:39:53 -0300

    feat(Inicia esqueleto aplicação)

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -3,17 +3,31 @@
     <button class="toggle-button" @click="toggleSidebar">
       {{ isCollapsed ? "☰" : "✕" }}
     </button>
-    <img src="/public/icons/favicon-96.png" alt="Logo" class="sidebar-logo" />
     <div v-if="!isCollapsed">
+      <img
+        src="/src/assets/icons/favicon-96.png"
+        alt="Logo"
+        class="sidebar-logo"
+      />
       <h2>Menu</h2>
       <ul>
         <li><router-link to="/home">Home</router-link></li>
-        <li>
-          <router-link to="/lista-clientes">Lista de Clientes</router-link>
+        <li @click="toggleClientesMenu" class="menu-item">
+          Clientes
+          <span class="arrow">{{ isClientesMenuOpen ? "▼" : "▶" }}</span>
         </li>
-        <li>
-          <router-link to="/cadastro-cliente">Cadastro de Cliente</router-link>
-        </li>
+        <ul v-if="isClientesMenuOpen" class="submenu">
+          <li>
+            <router-link to="/lista-clientes">Lista de Clientes</router-link>
+          </li>
+          <li>
+            <router-link to="/cadastro-cliente"
+              >Cadastro de Cliente</router-link
+            >
+          </li>
+        </ul>
+        <!-- <li><a href="#">Produtos</a></li>
+        <li><a href="#">Fornecedores</a></li> -->
       </ul>
     </div>
   </div>
@@ -24,12 +38,19 @@ export default {
   name: "Sidebar",
   data() {
     return {
-      isCollapsed: window.innerWidth <= 768, // Define o menu como colapsado em telas menores
+      isCollapsed: window.innerWidth <= 768,
+      isClientesMenuOpen: false,
     };
   },
   methods: {
     toggleSidebar() {
       this.isCollapsed = !this.isCollapsed;
+    },
+    toggleClientesMenu() {
+      this.isClientesMenuOpen = !this.isClientesMenuOpen;
+    },
+    handleResize() {
+      this.isCollapsed = window.innerWidth <= 768;
     },
   },
   mounted() {
@@ -37,14 +58,6 @@ export default {
   },
   beforeDestroy() {
     window.removeEventListener("resize", this.handleResize);
-  },
-  methods: {
-    handleResize() {
-      this.isCollapsed = window.innerWidth <= 768;
-    },
-    toggleSidebar() {
-      this.isCollapsed = !this.isCollapsed;
-    },
   },
 };
 </script>
@@ -65,6 +78,11 @@ export default {
 .sidebar h2 {
   color: #ecf0f1;
 }
+.sidebar-logo {
+  width: 100%;
+  max-width: 100px;
+  margin-bottom: 10px;
+}
 .sidebar ul {
   list-style-type: none;
   padding: 0;
@@ -78,6 +96,25 @@ export default {
 }
 .sidebar ul li a:hover {
   color: #3498db;
+}
+.submenu {
+  list-style-type: none;
+  padding-left: 20px; /* Ajuste para a direita */
+  margin: 5px; /* Espaço acima e abaixo do submenu */
+  margin-left: 20px; /* Espaço acima e abaixo do submenu */
+  padding-top: 5px; /* Espaço acima do submenu */
+  padding-bottom: 5px; /* Espaço abaixo do submenu */
+  transition: margin-left 0.3s ease; /* Transição suave ao abrir o submenu */
+}
+.submenu li {
+  margin-left: 20px; /* Move os itens do submenu para a direita */
+}
+.menu-item {
+  cursor: pointer; /* Indica que é clicável */
+}
+.arrow {
+  margin-left: 5px; /* Espaço entre o texto e a seta */
+  font-size: 0.7em; /* Tamanho menor para a seta */
 }
 .toggle-button {
   position: absolute;
